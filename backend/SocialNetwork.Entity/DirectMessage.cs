aa42d5ea960108ebb370f1db8b69d39477d2fab0
AmZu2024 <amar.zukanovic57@gmail.com>
2025-11-27 14:53:44 +0100

    Created a DirectMessage Entity.
    - Sender and RecieverId : we alwasys save the users ID and not the entire objet
    
    - Timestamp: timestamp when the message was sent
    
    -IsRead: To mark if the latest message was read
    
    - Navigation porperties: Will make it easier to get some properties such as Sender.User

---
+++ backend/SocialNetwork.Entity/DirectMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Entity
+{
+    public class DirectMessage
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+        public bool IsRead { get; set; } = false;
+
+        
+        public ApplicationUser Sender { get; set; }
+        public ApplicationUser Receiver { get; set; }
+
+    }
+}
