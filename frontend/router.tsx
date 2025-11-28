8ac327c74dd15b9c96315faa9fbae94a7f2dd846
kodpelle <pellewiklund@outlook.com>
2025-11-28 15:13:32 +0100

    Add LoginPage component and update router configuration

--- frontend/router.tsx
+++ frontend/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import AppLayout from "./layouts/AppLayout";
+import LoginPage from "./pages/LoginPage";
 
 export const router = createBrowserRouter([
     {
@@ -13,6 +14,10 @@ export const router = createBrowserRouter([
             {
                 path: "/register",
                 element: <RegisterPage />
+            },
+            {
+                path: "/login",
+                element: <LoginPage />
             }
         ]
     }
