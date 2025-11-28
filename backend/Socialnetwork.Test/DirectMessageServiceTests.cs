a88a47959e802aa4d01edff116d5dfc018a504e7
AmZu2024 <amar.zukanovic57@gmail.com>
2025-11-28 10:15:09 +0100

    Added failing test for empty message validation (RED step)

---
+++ backend/Socialnetwork.Test/DirectMessageServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SocialNetwork.Entity;
+using SocialNetwork.Repository;
+using Xunit;
+
+public class DirectMessageServiceTests
+{
+    [Fact]
+    public async Task CreateMessageShouldThrowWhenMessageIsEmpty()
+    {
+        // Arrange
+        var repoMock = new Mock<IDirectMessageRepository>();
+
+        var service = new DirectMessageService(repoMock.Object);
+
+        var message = new DirectMessage
+        {
+            SenderId = "user1",
+            ReceiverId = "user2",
+            Message = ""  
+        };
+
+        // Act & Assert
+        await Assert.ThrowsAsync<ArgumentException>(() =>
+            service.CreateMessageAsync(message)
+        );
+    }
+}
