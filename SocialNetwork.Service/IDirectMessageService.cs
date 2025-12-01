17b57d2f5906485f5b87579017729180cbaceb85
AmZu2024 <amar.zukanovic57@gmail.com>
2025-12-01 22:34:19 +0100

    Created IDirectMessageService interface with CreateMessageAsync method

---
+++ SocialNetwork.Service/IDirectMessageService.cs
@@ -0,0 +1,11 @@
+﻿using SocialNetwork.Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Service
+{
+    internal interface IDirectMessageService
+    {
+        Task<DirectMessage> CreateMessageAsync(DirectMessage message);
+    }
+}
