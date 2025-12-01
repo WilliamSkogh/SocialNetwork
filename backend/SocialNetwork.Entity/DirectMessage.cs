14421083b3433ad9d69e7e2733c633970e9305ce
AmZu2024 <amar.zukanovic57@gmail.com>
2025-12-01 22:23:07 +0100

    The senderId and ReceiverId are now strings so that they match the IdentityUser (foreignkey).

--- backend/SocialNetwork.Entity/DirectMessage.cs
+++ backend/SocialNetwork.Entity/DirectMessage.cs
@@ -9,8 +9,8 @@ namespace SocialNetwork.Entity
     public class DirectMessage
     {
         public int Id { get; set; }
-        public int SenderId { get; set; }
-        public int ReceiverId { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
         public string Message { get; set; }
         public DateTime Timestamp { get; set; } = DateTime.Now;
         public bool IsRead { get; set; } = false;
