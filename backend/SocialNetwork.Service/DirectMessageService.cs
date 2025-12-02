0fde782f434d1ce9f4de464743932df6d38e4534
AmZu2024 <amar.zukanovic57@gmail.com>
2025-12-02 11:44:49 +0100

    (GREEN) If  message SenderId is null throw new ArgumentException

--- backend/SocialNetwork.Service/DirectMessageService.cs
+++ backend/SocialNetwork.Service/DirectMessageService.cs
@@ -20,6 +20,8 @@ namespace SocialNetwork.Service
         {  
             if (string.IsNullOrWhiteSpace(message.Message))
                 throw new ArgumentException("Message content cannot be empty");
+            if (message.SenderId == null)
+                throw new ArgumentException("SenderId cannot be null");
 
             return await _repo.CreateAsync(message);
         }
