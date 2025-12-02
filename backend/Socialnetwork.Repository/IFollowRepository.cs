58931fa4659a37ae9e24509e7a5810d77dd96cfa
kodpelle <pellewiklund@outlook.com>
2025-12-02 10:59:47 +0100

    Interface for Moq

---
+++ backend/Socialnetwork.Repository/IFollowRepository.cs
@@ -0,0 +1,12 @@
+﻿using SocialNetwork.Entity;
+
+namespace Socialnetwork.Repository
+{
+    public interface IFollowRepository
+    {
+        Task<bool> IsFollowingAsync(string followerId, string followingId);
+        Task<bool> AddFollowAsync(Follow follow);
+        Task<bool> SaveChangesAsync();
+        Task<ApplicationUser> GetUserByIdAsync(string userId);
+    }
+}
