d881c0ae284bf6cff6bb219f3b1c106803330647
kodpelle <pellewiklund@outlook.com>
2025-12-02 13:30:05 +0100

    Add remove follow for interface

--- backend/Socialnetwork.Repository/IFollowRepository.cs
+++ backend/Socialnetwork.Repository/IFollowRepository.cs
@@ -8,5 +8,8 @@ namespace Socialnetwork.Repository
         Task<bool> AddFollowAsync(Follow follow);
         Task<bool> SaveChangesAsync();
         Task<ApplicationUser> GetUserByIdAsync(string userId);
+
+        Task<Follow?> GetFollowAsync(string followerId, string followingId);
+        Task RemoveFollowAsync(Follow follow);
     }
 }
