ec12037a3fe3ac92bfadcf7d02406329d3f26674
kodpelle <pellewiklund@outlook.com>
2025-12-03 14:45:49 +0100

    logoutbutton  only shows up if user is logged in

--- frontend/src/components/logoutButton.tsx
+++ frontend/src/components/logoutButton.tsx
@@ -2,7 +2,11 @@ import { useAuth } from "../AuthContext";
 import { Button } from "react-bootstrap";
 
 export default function LogoutButton() {
+    const { user } = useAuth();
     const { logout } = useAuth();
+    if (!user) {
+        return null;
+    }
 
     const handleLogout = async () => {
         await logout();
