287692ec7a3ccc62223408e6db3fef6071aeb4a2
kodpelle <pellewiklund@outlook.com>
2025-12-04 12:57:06 +0100

    add profile service

---
+++ backend/SocialNetwork.Service/ProfileService.cs
@@ -0,0 +1,18 @@
+﻿using Socialnetwork.Repository.Profile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Service;
+
+public class ProfileService
+{
+    private readonly IProfileRepository _repo;
+    public ProfileService(IProfileRepository repo)
+    {
+        _repo = repo;
+    }
+
+}
