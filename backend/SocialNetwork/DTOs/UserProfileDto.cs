7ddb4eb753f225d9aa150cf9e4ee6edac5c867dd
kodpelle <pellewiklund@outlook.com>
2025-12-04 14:57:17 +0100

    Updateprofile request

--- backend/SocialNetwork/DTOs/UserProfileDto.cs
+++ backend/SocialNetwork/DTOs/UserProfileDto.cs
@@ -1,12 +1,2 @@
-﻿namespace SocialNetwork.Api.DTOs
-{
-    public class UserProfileDto
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string? Bio { get; set; }
-        public string? ProfileImageUrl { get; set; }
-        public int FollowerCount { get; set; }
-        public int FollowingCount { get; set; }
-
-    }
-}
+﻿namespace SocialNetwork.Api.DTOs;
+public record UpdateProfileRequest(string Bio, string ImageUrl);
