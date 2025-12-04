dd94ecc365ea423a12f11d3d500d22de60b017ae
kodpelle <pellewiklund@outlook.com>
2025-12-04 14:57:37 +0100

    Endpoints to load profiles and update personal profile

---
+++ backend/SocialNetwork/Endpoints/Profiles/GetProfile.cs
@@ -0,0 +1,23 @@
+﻿using SocialNetwork.Api.Abstractions;
+using SocialNetwork.Service;
+
+namespace SocialNetwork.Api.Endpoints.Profiles;
+
+public class GetProfile : IEndpoint
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/users/{username}", async (string username, IProfileService profileService) =>
+        {
+            var profile = await profileService.GetUserProfileAsync(username);
+
+            if (profile == null)
+            {
+                return Results.NotFound(new { Message = "Användaren hittades inte." });
+            }
+            return Results.Ok(profile);
+        })
+            .WithTags("Profiles")
+            .RequireAuthorization();
+    }
+}
---
+++ backend/SocialNetwork/Endpoints/Profiles/UpdateProfile.cs
@@ -0,0 +1,42 @@
+﻿using SocialNetwork.Api.Abstractions;
+using SocialNetwork.Api.DTOs;
+using SocialNetwork.Service;
+using System.Security.Claims;
+
+namespace SocialNetwork.Api.Endpoints.Profiles;
+
+public class UpdateProfile : IEndpoint
+{
+    public static void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/api/users/{username}", async (
+            string username,
+            UpdateProfileRequest request,
+            ClaimsPrincipal user,
+            IProfileService profileService) =>
+        {
+
+            var loggedInUserName = user.FindFirstValue(ClaimTypes.Name);
+
+            if (loggedInUserName is null || !loggedInUserName.Equals(username, StringComparison.OrdinalIgnoreCase))
+            {
+                return Results.Forbid(); 
+            }
+            try
+            {
+                await profileService.UpdateUserProfileAsync(username, request.Bio, request.ImageUrl);
+                return Results.Ok(new { Message = "Profilen har uppdaterats!" });
+            }
+            catch (ArgumentException ex)
+            {
+                return Results.BadRequest(new { Error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem("Ett oväntat fel inträffade." +ex);
+            }
+        })
+        .WithTags("Profiles")
+        .RequireAuthorization();
+    }
+}
