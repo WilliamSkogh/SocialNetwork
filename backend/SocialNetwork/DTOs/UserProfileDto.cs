bae48c2a7873aed25dd86b5a9cc73a642a257f74
kodpelle <pellewiklund@outlook.com>
2025-12-04 11:51:09 +0100

    DTO for userprofile

---
+++ backend/SocialNetwork/DTOs/UserProfileDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialNetwork.Api.DTOs
+{
+    public class UserProfileDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string? Bio { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public int FollowerCount { get; set; }
+        public int FollowingCount { get; set; }
+
+    }
+}
