8043eb38b54c12ab1ea65753d2efb87143afa444
William <skogh22@gmail.com>
2025-12-05 09:25:16 +0100

    Add test for adding like to post

--- backend/Socialnetwork.Test/PostTests/PostLikeTests.cs
+++ backend/Socialnetwork.Test/PostTests/PostLikeTests.cs
@@ -14,4 +14,16 @@ public class PostLikeTests
         post.Likes.Should().NotBeNull();
         post.Likes.Should().BeEmpty();
     }
+
+    [Fact]
+    public void Post_ShouldAllowAddingLike()
+    {
+        var post = new Post { Id = 1 };
+        var like = new Like { PostId = 1, UserId = "user123" };
+        
+        post.Likes.Add(like);
+        
+        post.Likes.Should().HaveCount(1);
+        post.Likes.First().UserId.Should().Be("user123");
+    }
 }
