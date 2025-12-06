9785786ec6ee6ec73ea63c7cfc52a89a8863eb72
AmZu2024 <amar.zukanovic57@gmail.com>
2025-12-06 10:20:46 +0100

    Structuring DTOS to achieve: - Separation of concerns - Avoid null/unused fields - Easier mapping so each endpoint maps only the data it needs. -Clearer API contract so that frontend knows exactly what to expect per endpoint

--- backend/SocialNetwork/DTOs/MessageDTO.cs
+++ backend/SocialNetwork/DTOs/MessageDTO.cs
@@ -1,6 +1,23 @@
 ﻿namespace SocialNetwork.Api.DTOs
 {
-    public class DirectMessageDto
+
+    public class DirectMessageCreateResultDto
+    {
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool IsRead { get; set; }
+    }
+
+    public class DirectMessageCreateDto
+    {
+        public string ReceiverId { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class DirectMessageConversationDto
     {
         public int Id { get; set; }
         public string SenderId { get; set; }
@@ -10,13 +27,20 @@
         public string Message { get; set; }
         public DateTime Timestamp { get; set; }
         public bool IsRead { get; set; }
-        public int UnreadCount { get; set; }
     }
 
-    public class DirectMessageCreateDto
+    public class InboxMessageDto
     {
-        public string ReceiverId { get; set; }
+        public int Id { get; set; }
+        public string SenderId { get; set; }
+        public string SenderUsername { get; set; }
         public string Message { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int UnreadCount { get; set; }
+    }
+    public class UnreadCountDto
+    {
+        public int UnreadCount { get; set; }
     }
 
 
