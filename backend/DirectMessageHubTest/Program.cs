18de86c21c209f9166cfa113cff920045e3e095e
AmZu2024 <amar.zukanovic57@gmail.com>
2025-12-07 17:49:09 +0100

    removed user information in test

--- backend/DirectMessageHubTest/Program.cs
+++ backend/DirectMessageHubTest/Program.cs
@@ -6,13 +6,13 @@ Console.WriteLine("=== SignalR DirectMessage Hub Test ===\n");
 const string API_URL = "https://localhost:7166/hubs/direct-messages";
 
 // JWT Tokens (byt till dina)
-const string USER1_TOKEN = "CfDJ8K4FrlXyvKpGpabXYCrLKZdybDFwBNDqRrsReEUeq17zm6Eynen3sp8dMYzMG6lC3DAOhgZqUvFsG008sInKu6DTnVJp10K9UjTpzhkYL2qdx-d-H0P0gfWcJIXx9xG3q0lT66cLX9kryFiTOYzmbj6LBQXZoTnfHCM9vzfuLASLEhgWrH7h_KYun7hS4Ci0EpufD64f5nRrTso8RTX8aKdHPmV2BRBQ0359MrQWn2vEF9KWKrsVC7eLA-dV-YxqfMv7OG34maE2AaLmZ3Kx1l2IxdyxdBjkgvlRUQsPZCp3E4WqhDxZIJhtnteNlsybvjTgYxiVV6fB8JfQhfsAlZsZyyL9yNrOIxDBNOEMyYly85cSddzhxMkf95C0FZaG3ureeVk0MhItMiZUKmA1MU1fJhawX22S9OnbD5jOmgJT9GTl6dejtP_fXizzDcs75XfmXw8AJZZ-Fqs6SMhjULpuet5PdjdtArOhk1qiww_ePWbV-7XmUHRU63OhhTeQT02gLRhsVTjL4wKLgYDSc279mB2NSQWpjD9Mpx0cy0meJaMklrRxlobK7MyGUKBk4Yf7Sp2hzlWISHhEzxZ_Jtu9MSZxUkeQG5689hhuUQdv2ZQCdnKuwvtpX112oFi0fsGCgB-bjZLZLE_cDfI4eJb6SyqlBQNblKP8fbXTggLS";
-const string USER1_ID = "2931caeb-e162-42b9-bbcf-88be0fd7a83c";
-const string USER1_NAME = "Amar";
+const string USER1_TOKEN = "";
+const string USER1_ID = "";
+const string USER1_NAME = "";
 
-const string USER2_TOKEN = "CfDJ8K4FrlXyvKpGpabXYCrLKZeFSUexZZewYAL8SvRxoUAa5JkGPuD2fiMeEWGEUCx4FQVltDagld2jJ1kwlFo9UjtX6V-dfj7-xdvhRlsqyympURR_DNr-_32nDLBj2mWkhNm0973xtFPTKTPc9eBQ_7OxEI3cEkZS3B6jkvxLM2l98JdCfe6yJ3cx0JmXw8YDpAxYM089x9uMMBIVw9LdMd_ZIdNkLhNBnkRXowsu5nFgYCVqotxzk8VqIHbpiAuGL6erZkb1wHSdP6a7f10ARYiKvELR0jj_adgC9IPXvO1X02LhsOMbPcrgifzkF81yFo6rSbQUBH5wCmNcLoFOYDeMeFswMA79OraVf9ETd1XKPRYcnxTTnJKhK-UpxvB5PN3q_H1WGYaPqWkzRnaSCHSD1MYh2JgP4JeYb1VG5-jrY6faPkntXOIk_Kw9YhClJOd8Ju60i_HhIqvo6VB4644rhS0ztiz1FZEhauWBGFaHnsk_6h2AhFAwMAAVhJGU88dvcKnvriiINtbXKKI6yudxNgHl0-Rn5EIFVi9WdLtEAKpKwSUofNmoqg9hzt2EG3M6KJFh1i7m5iqnYaXHCDZFKhp__tS_pz_eBOCIal655_g7zsJNdIwZA-r2SuRI8Qzdo3ispsZUiNihjmApUGEkwTljIKMipJsGQI8FzznfaylPB3qUn1-HP2A5r4MYxw";
-const string USER2_ID = "4a4c1a33-de83-445c-b8d0-73922db84429";
-const string USER2_NAME = "Pelle";
+const string USER2_TOKEN = "";
+const string USER2_ID = "";
+const string USER2_NAME = "";
 
 var httpHandler = new HttpClientHandler();
 httpHandler.ServerCertificateCustomValidationCallback = (_, _, _, _) => true;
