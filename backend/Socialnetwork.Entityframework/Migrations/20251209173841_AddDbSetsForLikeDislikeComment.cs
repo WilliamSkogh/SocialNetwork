00ad1019871a409636c2ca2f2f71b3e37e757e10
William <skogh22@gmail.com>
2025-12-09 18:46:25 +0100

    db migrations and fixes

---
+++ backend/Socialnetwork.Entityframework/Migrations/20251209173841_AddDbSetsForLikeDislikeComment.cs
@@ -0,0 +1,300 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Socialnetwork.Entityframework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDbSetsForLikeDislikeComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Posts_PostId",
+                table: "Comment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dislike_AspNetUsers_UserId",
+                table: "Dislike");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dislike_Posts_PostId",
+                table: "Dislike");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Like_AspNetUsers_UserId",
+                table: "Like");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Like_Posts_PostId",
+                table: "Like");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Like",
+                table: "Like");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Dislike",
+                table: "Dislike");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment");
+
+            migrationBuilder.RenameTable(
+                name: "Like",
+                newName: "Likes");
+
+            migrationBuilder.RenameTable(
+                name: "Dislike",
+                newName: "Dislikes");
+
+            migrationBuilder.RenameTable(
+                name: "Comment",
+                newName: "Comments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Like_UserId",
+                table: "Likes",
+                newName: "IX_Likes_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Like_PostId",
+                table: "Likes",
+                newName: "IX_Likes_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dislike_UserId",
+                table: "Dislikes",
+                newName: "IX_Dislikes_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dislike_PostId",
+                table: "Dislikes",
+                newName: "IX_Dislikes_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_UserId",
+                table: "Comments",
+                newName: "IX_Comments_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comment_PostId",
+                table: "Comments",
+                newName: "IX_Comments_PostId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Likes",
+                table: "Likes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Dislikes",
+                table: "Dislikes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dislikes_AspNetUsers_UserId",
+                table: "Dislikes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dislikes_Posts_PostId",
+                table: "Dislikes",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_AspNetUsers_UserId",
+                table: "Likes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Likes_Posts_PostId",
+                table: "Likes",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dislikes_AspNetUsers_UserId",
+                table: "Dislikes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Dislikes_Posts_PostId",
+                table: "Dislikes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_AspNetUsers_UserId",
+                table: "Likes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Likes_Posts_PostId",
+                table: "Likes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Likes",
+                table: "Likes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Dislikes",
+                table: "Dislikes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Comments",
+                table: "Comments");
+
+            migrationBuilder.RenameTable(
+                name: "Likes",
+                newName: "Like");
+
+            migrationBuilder.RenameTable(
+                name: "Dislikes",
+                newName: "Dislike");
+
+            migrationBuilder.RenameTable(
+                name: "Comments",
+                newName: "Comment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Likes_UserId",
+                table: "Like",
+                newName: "IX_Like_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Likes_PostId",
+                table: "Like",
+                newName: "IX_Like_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dislikes_UserId",
+                table: "Dislike",
+                newName: "IX_Dislike_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Dislikes_PostId",
+                table: "Dislike",
+                newName: "IX_Dislike_PostId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_UserId",
+                table: "Comment",
+                newName: "IX_Comment_UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Comments_PostId",
+                table: "Comment",
+                newName: "IX_Comment_PostId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Like",
+                table: "Like",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Dislike",
+                table: "Dislike",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Comment",
+                table: "Comment",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_AspNetUsers_UserId",
+                table: "Comment",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Posts_PostId",
+                table: "Comment",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dislike_AspNetUsers_UserId",
+                table: "Dislike",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Dislike_Posts_PostId",
+                table: "Dislike",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Like_AspNetUsers_UserId",
+                table: "Like",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Like_Posts_PostId",
+                table: "Like",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
